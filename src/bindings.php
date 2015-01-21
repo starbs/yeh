e621dfc084df868fc4b4daff27b9a55836c1d2df
Graham Campbell <graham@mineuk.com>
2015-01-21 23:15:13 +0000

    Updated license headers

--- src/bindings.php
+++ src/bindings.php
@@ -1,15 +1,13 @@
 <?php
 
 /*
- * This file is part of Starbs Yeh by Graham Campbell.
+ * This file is part of Starbs Yeh.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Michael Banks <chip@mineuk.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 $app['hash'] = function () {
