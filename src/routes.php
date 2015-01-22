3733d2b77bdcd94f3c4134d3992a6224049427f1
Chip ‮♥ <chip@mineuk.com>
2015-01-22 12:36:37 +0000

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -15,3 +15,5 @@ $app->get('/', 'Starbs\Yeh\Http\Controllers\HomeController::index');
 $app->post('/', 'Starbs\Yeh\Http\Controllers\UploadController::index');
 
 $app->get('/{id}', 'Starbs\Yeh\Http\Controllers\MainController::index');
+
+$app->remove('/{id}', 'Starbs\Yeh\Http\Controllers\RemoveController::index');
